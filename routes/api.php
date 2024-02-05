4a592b9665ee84846f1e96d82bba7005295884a9
jsoldado <jsoldado@iesruizgijon.com>
2024-02-05 20:06:23 +0100

    Añadidas rutas de register, login , me

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ArticuloController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -35,6 +36,15 @@ Route::delete('/test', function () {
 
 //////////////////////////
 
+Route::post('/register', [AuthController::class, 'register']);
+
+Route::post('/login', [AuthController::class, 'login']);
+
+//Route::get('/me', [AuthController::class, 'me']);
+
+Route::get('/me', [AuthController::class, 'me'])->middleware('auth:sanctum');
+
+/////////////////////////////
 
 Route::resource('/articulos', ArticuloController::class); 
 
